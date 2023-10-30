a7ad4a33ffd36e4ed54259d0d641d1e43b4b436f
Tj Barrett <71855260+Tj-Barrett@users.noreply.github.com>
2023-10-30 13:14:05 +0100

    Add files via upload

--- python/bonds.py
+++ python/bonds.py
@@ -14,6 +14,9 @@ def moving_average(x,w):
 parser = argparse.ArgumentParser(description='Plot angle distributions from gromacs xvg')
 parser.add_argument('-f', help='Filename (.xvg)', type=argparse.FileType('r'), nargs='+')
 parser.add_argument('-l', help='Filename labels for the plot.', nargs='+')
+parser.add_argument('-n', help='Normalize.')
+parser.add_argument('-xlim', help='X axis limits.', nargs=2)
+parser.add_argument('-ylim', help='Y axis limits.', nargs=2)
 args = parser.parse_args()
 
 colors = "bgrcmykwbgrcmykw"
@@ -27,11 +30,22 @@ for _i, f in enumerate(args.f):
 
 	bond = [0]*len(lines)
 	quant = [0]*len(lines)
+	if args.n is not None:
+		qsum = 0.
 
 	for i, _l in enumerate(lines):
 		_sl = _l.split()
 		bond[i] = float(_sl[0])
 		quant[i] = float(_sl[1])
+		if args.n is not None:
+			qsum+= float(_sl[1])
+
+	if args.n is not None:
+		_quant = quant
+		del quant
+		quant = [0]*len(lines)
+		for i, _q in enumerate(_quant):
+			quant[i] = _q/qsum
 
 	if flabels is not None:
 		ax.plot(bond,quant, colors[_i], label=flabels[_i])
@@ -41,8 +55,20 @@ for _i, f in enumerate(args.f):
 handles, labels = ax.get_legend_handles_labels()
 
 plt.xlabel('Distance [nm]')
-plt.ylabel('Count')
-# plt.ylim([1.5, 1.7])
+
+if args.n is not None:
+	plt.ylabel('Probability Density')
+else:
+	plt.ylabel('Count')
+
+
+if args.xlim is not None:
+	_xlim = [float(args.xlim[0]),float(args.xlim[1])]
+	plt.xlim(_xlim)
+
+if args.ylim is not None:
+	_ylim = [float(args.ylim[0]),float(args.ylim[1])]
+	plt.ylim(_ylim)
 # ax.set_aspect('equal', adjustable='box')
 if flabels is not None:
 	ax.legend(handles, labels)
